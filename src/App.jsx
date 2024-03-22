ca121317d92b1f8d6bfcc80aa6fd951e19f1f4a4
YuliiaLukomska <142302232+YuliiaLukomska@users.noreply.github.com>
2024-03-22 16:09:29 +0200

    add nav and routes and pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,35 @@
+import { NavLink, Route, Routes } from "react-router-dom";
+import clsx from "clsx";
+import css from "./App.module.css";
+import HomePage from "./pages/HomePage/HomePage";
+import { MoviesPage } from "./pages/MoviesPage/MoviesPage";
+import MovieDetailsPage from "./pages/MovieDetailsPage/MovieDetailsPage";
+import { NotFoundPage } from "./pages/NotFoundPage/NotFoundPage";
+
 function App() {
+  const activeLinkClass = ({ isActive }) => {
+    return clsx({ [css.active]: isActive });
+  };
+
   return (
     <>
-      <div></div>
+      <div>
+        <nav>
+          <NavLink to="/" className={activeLinkClass}>
+            Home
+          </NavLink>
+          <NavLink to="/movies" className={activeLinkClass}>
+            Movies
+          </NavLink>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<MoviesPage />} />
+          <Route path="/movies/:movieId" element={<MovieDetailsPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
     </>
   );
 }
