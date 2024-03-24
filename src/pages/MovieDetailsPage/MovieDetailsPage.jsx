6c5ad6fed3514cdfb947609d2aab2ee1828d2ea4
YuliiaLukomska <142302232+YuliiaLukomska@users.noreply.github.com>
2024-03-24 08:45:55 +0200

    add styles

--- src/pages/MovieDetailsPage/MovieDetailsPage.jsx
+++ src/pages/MovieDetailsPage/MovieDetailsPage.jsx
@@ -3,6 +3,7 @@ import { Link, Route, Routes, useLocation, useParams } from "react-router-dom";
 import fetchFilmById from "../../services/fetchFilmById";
 import Error from "../../components/Error/Error";
 import Loader from "../../components/Loader/Loader";
+import css from "./MovieDetailsPage.module.css";
 
 const MovieCast = lazy(() => import("../../components/MovieCast/MovieCast"));
 const MovieReviews = lazy(() =>
@@ -45,23 +46,29 @@ const MovieDetailsPage = () => {
       {film !== null && (
         <div>
           <Link to={backLinkRef.current}>Go Back</Link>
-          <h1>{film.title}</h1>
-          <div>
+          <h1 className={css.filmMainTitle}>{film.title}</h1>
+          <div className={css.filmWrapper}>
             <img
               src={`https://image.tmdb.org/t/p/w500${film.backdrop_path}`}
               alt=""
             />
-            <div>
-              <p>{film.original_title}</p>
-              <p>Overview</p>
-              <p>{film.overview}</p>
-              <p>Popularity: {film.popularity}</p>
-              <p>Release date: {film.release_date}</p>
+            <div className={css.filmDescriptionWrapper}>
+              <p className={css.filmTitle}>{film.original_title}</p>
+              <p>
+                <b>Overview</b>
+              </p>
+              <p className={css.filmOverview}>{film.overview}</p>
+              <p className={css.filmRating}>
+                <b>Popularity:</b> {film.popularity}
+                <b>Release date:</b> {film.release_date}
+              </p>
             </div>
           </div>
+          <div className={css.filmAdditionalInfo}>
+            <Link to="cast">Cast</Link>
+            <Link to="reviews">Reviews</Link>
+          </div>
 
-          <Link to="cast">Cast</Link>
-          <Link to="reviews">Reviews</Link>
           <Suspense fallback={<Loader />}>
             <Routes>
               <Route path="cast" element={<MovieCast />} />
