9f95707b2ac272ccc4944f597f8021a0ac3b555c
YuliiaLukomska <142302232+YuliiaLukomska@users.noreply.github.com>
2024-03-24 09:03:37 +0200

    add link on notfounfpage

--- src/pages/NotFoundPage/NotFoundPage.jsx
+++ src/pages/NotFoundPage/NotFoundPage.jsx
@@ -1,5 +1,12 @@
+import { Link } from "react-router-dom";
+
 const NotFoundPage = () => {
-  return <div>NotFoundPage</div>;
+  return (
+    <div>
+      <p>Sorry...This page is not found</p>
+      <Link to="/">Home</Link>
+    </div>
+  );
 };
 
 export default NotFoundPage;
